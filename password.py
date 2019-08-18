999a5b77654cc9055c8efbcba39a8ddcef315668
Rex Hsu <weiyao220@gmail.com>
2019-08-18 14:10:10 +0800

    Adjusting else and if descriptions to improve the system

--- password.py
+++ password.py
@@ -1,11 +1,15 @@
 password = 'a123456'
 i = 3 #Chances Left
 while i > 0:
+	i = i - 1
 	pwd = input('Please enter your password: ')
 	if pwd == password:
 		print('Logged in Successfully')
 		break
 	else:
-		i = i - 1
-		print('Wrong Password, you still have', i, 'chances')
+		print('Wrong Password !')
+		if i > 0:
+			print('You still have', i, 'chances')
+		else: 
+			print('No chances left !')
 		 
